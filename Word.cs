fc30851ca92a88376cbc0fde4ed166bd89be0671
ani-mihaylova <ani.ad.lib@gmail.com>
2014-06-27 14:34:00 +0300

    Add some functionality in Word class

--- Word.cs
+++ Word.cs
@@ -4,22 +4,62 @@
 
     public class Word
     {
+        private const int MaxLengthOfLetter = 1;
         private string word;
         private char[] wordOnScreen;
 
         public Word(string word)
         {
-            throw new NotImplementedException();
+            this.Word = word;
+        }
+
+        public string Word
+        {
+            get { return this.word; }
+            private set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Word cannot be null!");
+                }
+
+                if (value == string.Empty)
+                {
+                    throw new ArgumentException("Word cannot be empty string!");
+                }
+            }
         }
 
         public bool WordContainsletter(string letter)
         {
-            throw new NotImplementedException();
+            if (letter==string.Empty)
+            {
+                throw new ArgumentException("Letter cannot be empty string!");
+            }
+
+            if (letter==null)
+            {
+                throw new ArgumentNullException("Letter cannot be null!");
+            }
+
+            if (letter.Length>MaxLengthOfLetter)
+            {
+                throw new ArgumentException();//TODO:need to change to more appropriate exception
+            }
+
+            bool result = this.Word.Contains(letter);
+
+            return result;
         }
 
         public bool WordIsGuessed(string wordToCompare)
         {
-            throw new NotImplementedException();
+            if (wordToCompare == this.Word)
+            {
+                return true;
+            }
+
+            return false;
         }
 
         public void UpdateWordOnScreen(string letter)
