b438af9ad5e909e5e7c3eb7d9d380bbb0bd83fe1
ani-mihaylova <ani.ad.lib@gmail.com>
2014-07-02 17:05:53 +0300

    Add comments

---
+++ Hangman.cs
@@ -0,0 +1,21 @@
+﻿//The Command pattern
+//Template Method pattern
+//Observer pattern?? UpdateWordOnScreen
+//Mediator pattern
+//Memento-Results??
+
+using System;
+using System.Linq;
+
+namespace Hangman
+{
+    public class Hangman
+    {
+        public static void Main(string[] args)
+        {
+            Player player=new Player("Pasho",30);
+            GameEngine gameEngine = new GameEngine(player);
+            gameEngine.Start();
+        }
+    }
+}
