19ec92cfaa7466612c5fc0e7b86100441db1f723
kalimar <kalimar@abv.bg>
2014-07-09 19:57:02 +0300

    Adding Enum extensions

---
+++ Extensions/EnumExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hangman.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static T ToEnum<T>(this string str, bool ignoreCase = true) where T : struct, IConvertible
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new ArgumentException("T must be an enumerated type");
+            }
+
+            return (T)Enum.Parse(typeof(T), str, ignoreCase);
+        }
+    }
+}
